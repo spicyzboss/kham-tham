eee0a6e2ede72ad763d7b916e5bca877d4979b98
spicyzboss <bossthth1209mine@gmail.com>
2022-10-19 13:56:40 +0700

    db: add example

--- packages/api/index.ts
+++ packages/api/index.ts
@@ -2,11 +2,8 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-prisma.user.create({
-  data: {
-    username: "spicyzboss",
-    password: "password",
-  }
+prisma.user.findMany({
+
 })
   .then(e => {
     console.log(e)
