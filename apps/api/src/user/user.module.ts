1b7d424347f9d13d476b9136e2ea211c853a3259
spicyzboss <bossthth1209mine@gmail.com>
2022-11-29 13:13:38 +0700

    auth system

--- apps/api/src/user/user.module.ts
+++ apps/api/src/user/user.module.ts
@@ -1,11 +1,18 @@
 import { Global, Module } from "@nestjs/common";
+import { JwtModule } from '@nestjs/jwt';
 import { PrismaModule } from "src/prisma/prisma.module";
 import { UserController } from "./user.controller";
 import { UserService } from "./user.service";
 
 @Global()
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    PrismaModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '1d' },
+    }),
+  ],
   controllers: [UserController],
   providers: [UserService],
 })
--- apps/api/src/user/user.service.ts
+++ apps/api/src/user/user.service.ts
@@ -1,25 +1,109 @@
 import { Injectable } from '@nestjs/common';
-import { User } from '@prisma/client';
+import { Prisma, User } from '@prisma/client';
+import { hash, compare } from 'bcrypt';
+import { CreateUserResponse, CreateUserRequest, UserResponse, LoginRequest } from 'types/user';
 import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class UserService {
   constructor(private prisma: PrismaService) { }
 
-  async getUserById(username: string): Promise<Pick<User, 'email' | 'username'>> {
+  async getUserById(id: string): Promise<UserResponse> {
     try {
-      return await this.prisma.user.findUnique({
+      const user = await this.prisma.user.findUnique({
         where: {
-          username
+          id
         },
         select: {
           email: true,
           username: true,
+          id: true,
         }
       });
-    } catch (e) {
-      console.log('UserService -> getUserById: Error', e.message);
 
+      return user;
+    } catch (e) {
+      return null;
+    }
+  }
+
+  async validateUsername(username: string): Promise<User> {
+    try {
+      const user = await this.prisma.user.findUnique({
+        where: {
+          username
+        },
+      });
+
+      return user;
+    } catch (e) {
+      return null;
+    }
+  }
+
+  async validateEmail(email: string): Promise<User> {
+    try {
+      const user = await this.prisma.user.findUnique({
+        where: {
+          email
+        },
+      });
+
+      return user;
+    } catch (e) {
+      return null;
+    }
+  }
+
+  async createUser(userArgs: CreateUserRequest): Promise<CreateUserResponse> {
+    const { email, password, username } = userArgs;
+
+    try {
+      const hashedPassword = await hash(password, 12);
+
+      const user = await this.prisma.user.create({
+        data: {
+          email,
+          username,
+          password: hashedPassword,
+        },
+        select: {
+          id: true,
+        }
+      });
+
+      return user;
+    } catch (e) {
+      return null;
+    }
+  }
+
+  async loginUser(cred: LoginRequest) {
+    try {
+      const user = await this.prisma.user.findFirst({
+        where: {
+          username: cred.username,
+          // password: hashedPassword,
+        },
+        select: {
+          id: true,
+          password: true,
+        }
+      });
+
+      console.log(user.password);
+      console.log(cred.password);
+
+      const isSame = await compare(cred.password, user.password);
+      console.log(isSame);
+
+      if (isSame) {
+        delete user.password;
+        return user;
+      } else {
+        return null;
+      }
+    } catch (e) {
       return null;
     }
   }
