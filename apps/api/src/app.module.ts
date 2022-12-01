396b5f0a1c89c2325b8534195dc787858315d66f
spicyzboss <bossthth1209mine@gmail.com>
2022-12-01 19:35:55 +0700

    schema add updateat

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,14 +1,12 @@
 import { Module } from '@nestjs/common';
 import { EventModule } from './event/event.module';
-import { HelloModule } from './hello/hello.module';
 import { RoomModule } from './room/room.module';
 import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
-    HelloModule,
-    RoomModule,
     UserModule,
+    RoomModule,
     EventModule,
   ],
 })
